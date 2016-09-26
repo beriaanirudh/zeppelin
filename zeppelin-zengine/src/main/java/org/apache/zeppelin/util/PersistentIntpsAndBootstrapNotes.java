8e581916555d963bb1b31e268694da8c65ece5f4
Beria <beria@qubole.com>
2016-09-26 13:35:03 +0530

    fix: usr: ZEP-195: NPE when starting server since no Notebook object is present

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/util/PersistentIntpsAndBootstrapNotes.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/util/PersistentIntpsAndBootstrapNotes.java
@@ -14,6 +14,7 @@ import org.apache.zeppelin.interpreter.remote.RemoteInterpreterProcess;
 import org.apache.zeppelin.interpreter.thrift.RemoteInterpreterService.Client;
 import org.apache.zeppelin.notebook.Note;
 import org.apache.zeppelin.notebook.NoteInterpreterLoader;
+import org.apache.zeppelin.notebook.Notebook;
 import org.apache.zeppelin.notebook.Notebook.CronJob;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,11 +32,8 @@ public class PersistentIntpsAndBootstrapNotes implements Runnable {
   private static final ScheduledExecutorService service =
       Executors.newSingleThreadScheduledExecutor();
 
-  private final InterpreterFactory intpFactory;
-
-  private PersistentIntpsAndBootstrapNotes(InterpreterFactory factory) {
-    intpFactory = factory;
-  }
+  private PersistentIntpsAndBootstrapNotes()
+  {}
 
   @Override
   public void run() {
@@ -43,17 +41,19 @@ public class PersistentIntpsAndBootstrapNotes implements Runnable {
      * stopping the scheduler, so we should Catch'em All.
      */
     try {
+      Notebook notebook = CronJob.notebook;
+      InterpreterFactory intpFactory = notebook.getInterpreterFactory();
       List<InterpreterSetting> settings = intpFactory.get();
       for (InterpreterSetting setting: settings) {
-        checkPersistence(setting, intpFactory, CronJob.notebook.getAllNotes());
+        checkPersistence(setting, intpFactory, notebook.getAllNotes());
       }
     } catch (Exception e) {
       LOG.error("Error while trying to start persistent interpreters", e);
     }
   }
 
-  public static void schedulePeristentInterpreters(InterpreterFactory factory) {
-    PersistentIntpsAndBootstrapNotes persistentIntp = new PersistentIntpsAndBootstrapNotes(factory);
+  public static void schedulePeristentInterpreters() {
+    PersistentIntpsAndBootstrapNotes persistentIntp = new PersistentIntpsAndBootstrapNotes();
     LOG.info("Starting a scheduler to start persistent interpreters and "
         + "run bootstrap notebooks");
     ZeppelinConfiguration conf = ZeppelinConfiguration.create();
