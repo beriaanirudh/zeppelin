84682bef7727a44ea9d60b2890236a14078c8d49
karuppayya <karuppayyar@qubole.com>
2016-11-21 17:27:24 +0530

    chg: dev: ZEP-338: Send the id(notes table) when sending events to tapp

--- zeppelin-zengine/src/main/java/org/apache/zeppelin/events/QuboleNoteEvent.java
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/events/QuboleNoteEvent.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import org.apache.zeppelin.events.QuboleEventsEnum.EVENTTYPE;
 import org.apache.zeppelin.notebook.Note;
+import org.apache.zeppelin.util.QuboleNoteAttributes;
 
 /**
  *
@@ -15,11 +16,19 @@ public class QuboleNoteEvent implements Serializable {
   private String name;
   private String zeppelin_id;
   private String source;
+  private String id;
 
   public QuboleNoteEvent(Note note, EVENTTYPE event) {
     name = note.getName();
     zeppelin_id = note.id();
     source = note.getSource();
+    QuboleNoteAttributes quboleNoteAttributes = note.getQuboleNoteAttributes();
+    if (quboleNoteAttributes != null) {
+      Integer noteId = quboleNoteAttributes.getNoteId();
+      if (noteId != null) {
+        id = noteId.toString();
+      }
+    }
   }
 
 }
