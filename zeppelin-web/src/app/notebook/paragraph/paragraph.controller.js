6a78cecb876bd57241591e62a1602d7a72f9fa10
Beria <beria@qubole.com>
2017-01-30 11:22:48 +0530

    Fix non-syncing of notes across web socket connections (browsers/tabs)

--- zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
+++ zeppelin-web/src/app/notebook/paragraph/paragraph.controller.js
@@ -381,7 +381,7 @@ angular.module('zeppelinWebApp')
             $scope.dirtyText = undefined;
             $scope.originalText = angular.copy(data.paragraph.text);
           } else { // if there're local update, keep it.
-            $scope.paragraph.text = $scope.dirtyText;
+            $scope.paragraph.text = data.paragraph.text;
           }
         } else {
           $scope.paragraph.text = data.paragraph.text;
